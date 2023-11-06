a1b906b1f4ad753cdc01f3b05c74babade6f2f0b
Bastien TAKIS <bastientakis@gmail.com>
2023-11-06 18:50:56 +0100

    feat: create file of company in public directory on click

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -14,5 +15,15 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    #[Route('/create-file', name: 'app_create_file')]
+    public function createCompanyFile(Request $request): Response
+    {
+
+        $data = json_decode($request->getContent(), true);
+        $company = $data['company'];
+        file_put_contents("./companies/company_" . $company["nom_raison_sociale"] . ".txt", $company["nom_raison_sociale"]);
+        return $this->json(["response" => $company]);
     }
 }
